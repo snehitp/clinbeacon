f0174715656d178fc8766ebd2a29ccec63c7fc6b
Trent Swanson <trentmswanson@hotmail.com>
2016-08-26 14:56:08 -0700

    update bootstrap

--- beacon/src/ui/app/import/import.component.ts
+++ beacon/src/ui/app/import/import.component.ts
@@ -5,5 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: '/app/import/import.component.html'
 })
 export class ImportComponent {
+
+  files = [{
+    id:"asf"
+  }]
   
+  delete(id:string) {
+
+  }
+
+  import() {
+
+  }
 }

cb7e88f688934975b0089764195c2e5293b63c64
Trent Swanson <trentmswanson@hotmail.com>
2016-08-26 07:04:01 -0700

    fix login in ui

--- beacon/src/ui/app/app.component.ts
+++ beacon/src/ui/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'angular2-cookie/core';
+import { AuthService } from './account/auth.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: '/app/app.component.html'
 })
 export class AppComponent {
+  constructor(private cookieService: CookieService, private authService: AuthService) { }
 
+  isAuth = false;
+
+  username() {
+    return this.authService.getUserName();
+  }
+
+  logout() {
+    this.cookieService.remove('session_id');
+    window.location.href = '/';
+  }
 }
--- beacon/src/ui/app/import/import.component.ts
+++ beacon/src/ui/app/import/import.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'vcf-import',
   template: `
-    <h2>Batch Import</h2>
-    <div>
-      This functionality has been moved to patient and is now a placeholder for batch import
-    </div>
+    <h2>VCF Data</h2>
     `
 })
 export class ImportComponent {

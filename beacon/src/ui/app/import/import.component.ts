94c646fe199b83cfe5964d83f08b82491945cac2
Trent Swanson <trentmswanson@hotmail.com>
2016-08-26 17:34:37 -0700

    update import ui

--- beacon/src/ui/app/import/import.component.ts
+++ beacon/src/ui/app/import/import.component.ts
@@ -28,11 +28,43 @@ export class ImportComponent {
 
   import() {
 
-    // We may want to consider one of the bootstrap libraries
-    document.getElementById("modalCancel").click();
+    this.isUploading = true;
+
+    this.upload(this.selectedImportFile.file[0])
+      .then(result => { this.isUploading = false; document.getElementById("modalCancel").click(); })
+      .catch(error => { console.log(error); this.isUploading = false })
   }
 
   cancelImport() {
     this.importFileName = "";
+  }
+
+    // Upload a vcf file
+  upload(file) {
+    return new Promise((resolve, reject) => {
+      let xhr: XMLHttpRequest = new XMLHttpRequest();
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
+        }
+      };
+
+      // update file upload progress
+      xhr.upload.onprogress = (event:any) => {
+        this.importProgress = Math.round(event.lengthComputable ? event.loaded * 100 / event.total : 0);
+      };
+
+      xhr.open('POST', '/api/import/vcf', true);
+
+      let formData = new FormData();
+
+      formData.append("file", file, this.importFileName);
+
+      xhr.send(formData);
+    });
   }
 }
